ac1698f37d0261fc53b34e65a411bf75c29a4d8e
Pedro Loures <pedroh.z.loures@gmail.com>
2026-04-19 20:45:14 -0400

    new tests

--- Jellyfin.Plugin.MergeEpisodes/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.MergeEpisodes/Configuration/PluginConfiguration.cs
@@ -1,23 +1,57 @@
-﻿using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Plugins;
 
-namespace Jellyfin.Plugin.MergeEpisodes.Configuration
+namespace Jellyfin.Plugin.MergeEpisodes.Configuration;
+
+/// <summary>
+/// The configuration options.
+/// </summary>
+public enum SomeOptions
 {
     /// <summary>
-    /// Plugin configuration.
+    /// Option one.
     /// </summary>
-    public class PluginConfiguration : BasePluginConfiguration
-    {
-        /// <summary>
-        /// Gets or sets the library paths excluded from merging.
-        /// </summary>
-        public string[] LocationsExcluded { get; set; }
+    OneOption,
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
-        /// </summary>
-        public PluginConfiguration()
-        {
-            LocationsExcluded = Array.Empty<string>();
-        }
+    /// <summary>
+    /// Second option.
+    /// </summary>
+    AnotherOption
+}
+
+/// <summary>
+/// Plugin configuration.
+/// </summary>
+public class PluginConfiguration : BasePluginConfiguration
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
+    /// </summary>
+    public PluginConfiguration()
+    {
+        // set default options here
+        Options = SomeOptions.AnotherOption;
+        TrueFalseSetting = true;
+        AnInteger = 2;
+        AString = "string";
     }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether some true or false setting is enabled..
+    /// </summary>
+    public bool TrueFalseSetting { get; set; }
+
+    /// <summary>
+    /// Gets or sets an integer setting.
+    /// </summary>
+    public int AnInteger { get; set; }
+
+    /// <summary>
+    /// Gets or sets a string setting.
+    /// </summary>
+    public string AString { get; set; }
+
+    /// <summary>
+    /// Gets or sets an enum option.
+    /// </summary>
+    public SomeOptions Options { get; set; }
 }
--- Jellyfin.Plugin.MergeEpisodes/Plugin.cs
+++ Jellyfin.Plugin.MergeEpisodes/Plugin.cs
@@ -1,60 +1,51 @@
-﻿using Jellyfin.Plugin.MergeEpisodes.Configuration;
-using MediaBrowser.Controller;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Jellyfin.Plugin.MergeEpisodes.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
-using MediaBrowser.Common.Plugins;
 
-namespace Jellyfin.Plugin.MergeEpisodes
+namespace Jellyfin.Plugin.MergeEpisodes;
+
+/// <summary>
+/// The main plugin.
+/// </summary>
+public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
     /// <summary>
-    /// Merge Episodes plugin entry point.
+    /// Initializes a new instance of the <see cref="Plugin"/> class.
     /// </summary>
-    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    /// <param name="applicationPaths">Instance of the <see cref="IApplicationPaths"/> interface.</param>
+    /// <param name="xmlSerializer">Instance of the <see cref="IXmlSerializer"/> interface.</param>
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+        : base(applicationPaths, xmlSerializer)
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Plugin"/> class.
-        /// </summary>
-        /// <param name="appPaths">Application paths.</param>
-        /// <param name="xmlSerializer">XML serializer.</param>
-        public Plugin(IServerApplicationPaths appPaths, IXmlSerializer xmlSerializer)
-            : base(appPaths, xmlSerializer)
-        {
-            Instance = this;
-        }
+        Instance = this;
+    }
 
-        /// <inheritdoc />
-        public override string Name => "Merge Episodes";
+    /// <inheritdoc />
+    public override string Name => "Merge Episodes";
 
-        /// <summary>
-        /// Gets the current plugin instance.
-        /// </summary>
-        public static Plugin? Instance { get; private set; }
+    /// <inheritdoc />
+    public override Guid Id => Guid.Parse("c5315368-51cd-4efd-b930-a9bef8a9e44f");
 
-        /// <inheritdoc />
-        public override string Description
-            => "Merge Episodes";
+    /// <summary>
+    /// Gets the current plugin instance.
+    /// </summary>
+    public static Plugin? Instance { get; private set; }
 
-        /// <summary>
-        /// Gets the plugin configuration.
-        /// </summary>
-        public PluginConfiguration PluginConfiguration => Configuration;
-
-        private readonly Guid _id = new Guid("f21bbed8-3a97-4d8b-88b2-48aaa65427cb");
-
-        /// <inheritdoc />
-        public override Guid Id => _id;
-
-        /// <inheritdoc />
-        public IEnumerable<PluginPageInfo> GetPages()
-        {
-            return new[]
+    /// <inheritdoc />
+    public IEnumerable<PluginPageInfo> GetPages()
+    {
+        return
+        [
+            new PluginPageInfo
             {
-                new PluginPageInfo
-                {
-                    Name = "Merge Episodes",
-                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.configurationpage.html"
-                }
-            };
-        }
+                Name = Name,
+                EmbeddedResourcePath = string.Format(CultureInfo.InvariantCulture, "{0}.Configuration.configPage.html", GetType().Namespace)
+            }
+        ];
     }
 }
